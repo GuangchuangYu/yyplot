2878b7d4a9ea9ddd08fbec223122cf37d38edb89
guangchuang yu <guangchuangyu@gamil.com>
2017-10-20 15:45:52 +0800

    meme_save

--- R/meme.R
+++ R/meme.R
@@ -25,26 +25,42 @@ meme <- function(img, upper="", lower="", size="auto", color="white", font="Helv
     x <- image_read(img)
     info <- image_info(x)
 
-    scale <- 250
     if (size == "auto") {
-        ## size <- info$height / 10
-        size <- info$height/scale
+        size <- info$height/250
     }
-    ## m <- (info$width - size)/2
-    ## u <- paste0("+", m, "+", info$height * .1)
-    ## l <- paste0("+", m, "+", info$height * .9)
-    ## x <- image_annotate(x, toupper(upper), size=size, location = u, color = color, font = font)
-    ## x <- image_annotate(x, toupper(lower), size=size, location = l, color = color, font = font)
 
-    upperGrob <- textGrob(toupper(upper), gp = gpar(col = color, fontfamily = font, cex = size), vp = viewport(y=.8))
-    lowerGrob <- textGrob(toupper(lower), gp = gpar(col = color, fontfamily = font, cex = size), vp = viewport(y=.1))
+    gp <- gpar(col = color, fontfamily = font, cex = size)
+    upperGrob <- textGrob(toupper(upper), gp = gp, vp = viewport(y=.8))
+    lowerGrob <- textGrob(toupper(lower), gp = gp, vp = viewport(y=.1))
 
-    res <- gList(rasterGrob(x), upperGrob, lowerGrob)
+    meme <- gList(rasterGrob(x), upperGrob, lowerGrob)
 
     if (plot) {
-        dev.new(width=info$width/scale, height=info$height/scale)
-        grid.draw(res)
+        dev.new(width=7, height=7*info$height/info$width)
+        grid.draw(meme)
     }
+
+    res <- list(gList=meme, width = info$width, height = info$height)
     invisible(res)
 }
 
+##' save meme plot
+##'
+##'
+##' @title meme_save
+##' @param meme meme output
+##' @param file output file
+##' @param device graphic device
+##' @param dpi dpi of the figure
+##' @return NULL
+##' @export
+##' @author guangchuang yu
+meme_save <- function(meme, file="meme.png", device=NULL, dpi = 300) {
+    dev <- plot_dev(device, file, dpi = dpi)
+    dev(file = file, width = meme$width * 0.010417, height = meme$height * 0.010417)
+    on.exit(utils::capture.output(grDevices::dev.off()))
+    grid.draw(meme$gList)
+    invisible()
+}
+
+plot_dev <- getFromNamespace("plot_dev", "ggplot2")
