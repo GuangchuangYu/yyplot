f3fb3460cda1f45ef2f4d5cfa91fc01bebda7c0b
guangchuang yu <guangchuangyu@gamil.com>
2017-11-08 18:15:34 +0800

    ggvenn

---
+++ R/ggvenn.R
@@ -0,0 +1,37 @@
+##' venn plot using ggplot2
+##'
+##'
+##' @title ggvenn
+##' @param x data
+##' @param alpha transparency of color
+##' @return ggplot object
+##' @importFrom venneuler venneuler
+##' @importFrom ggforce geom_circle
+##' @importFrom ggplot2 geom_text
+##' @importFrom ggplot2 coord_fixed
+##' @importFrom ggplot2 ggplot
+##' @importFrom ggplot2 aes_
+##' @export
+##' @author guangchuang yu
+##' @examples
+##' set.seed(2017-11-08)
+##' x <- matrix(sample(0:4, 40, TRUE, c(.5, .1, .1, .1, .1)), ncol=4)
+##' colnames(x) <- LETTERS[1:4]
+##' ggvenn(x)
+ggvenn <- function(x, alpha = 0.5) {
+    ## the venneuler depends on rJava
+    ## maybe wrapping VennDiagram (output gList) as geom layer
+    y <- venneuler(x)
+    d <- data.frame(y$centers,
+                    diameters = y$diameters,
+                    labels = y$labels,
+                    stringsAsFactors = FALSE)
+    ggplot(d) +
+        geom_circle(aes_(x0 = ~x, y0 = ~y,
+                         r = ~diameters/2, fill = ~labels),
+                    alpha = alpha) +
+        geom_text(aes_(x = ~x, y = ~y, label = ~labels)) +
+        coord_fixed()
+}
+
+
