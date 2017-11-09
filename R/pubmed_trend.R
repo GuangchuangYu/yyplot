a27f9328b6fafa20568bd35ed253d4f6c518c76b
guangchuang yu <guangchuangyu@gamil.com>
2017-11-09 21:05:32 +0800

    pubmed trend

---
+++ R/pubmed_trend.R
@@ -0,0 +1,58 @@
+##' query pubmed record numbers of search term through years
+##'
+##'
+##' @title pubmed_trend
+##' @param searchTerm search term
+##' @param year year vector
+##' @param verbose whether print message
+##' @return data.frame
+##' @author guangchuang yu
+##' @importFrom plyr ldply
+##' @export
+##' @examples
+##' \dontrun{
+##' pubmed_trend("Yu Guangchuang[Full Author Name]", 2010:2016)
+##' }
+pubmed_trend <- function(searchTerm, year, verbose=TRUE) {
+    if (length(searchTerm) == 1)
+        return(pubmed_trend.internal(searchTerm, year))
+    res <- lapply(searchTerm, pubmed_trend.internal, year=year)
+    names(res) <- searchTerm
+    res.df <- ldply(res)
+    colnames(res.df)[1] <- "TERM"
+    class(res.df) <- "pubmedTrend"
+    return(res.df)
+}
+
+##' @importFrom RISmed EUtilsSummary
+##' @importFrom RISmed QueryCount
+pubmed_trend.internal <- function(searchTerm, year, verbose=TRUE) {
+    num <- array()
+    x <- 1
+    if (verbose)
+        message('search term: ', searchTerm)
+
+    for (i in year){
+        Sys.sleep(1)
+        if (verbose)
+            message("-> querying year ", i)
+        r <- EUtilsSummary(searchTerm, type='esearch', db='pubmed', mindate=i, maxdate=i)
+        num[x] <- QueryCount(r)
+        x <- x + 1
+    }
+    res <- data.frame(year=year, number=num)
+    return(res)
+}
+
+##' @method plot pubmedTrend
+##' @export
+##' @importFrom ggplot2 geom_line
+plot.pubmedTrend <- function(x, y, ...) {
+    class(x) <- "data.frame"
+    if ('TERM' %in% colnames(x)) {
+        mapping <- aes_(x = ~year, y = ~number, group = ~TERM, color = ~TERM)
+    } else {
+        mapping <- aes_(x = ~year, y = ~number)
+    }
+    ggplot(x, mapping) + geom_line() + geom_point()
+}
