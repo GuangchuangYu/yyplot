ad2073f4f84748e6689e22871eec62f571c061fd
guangchuang yu <guangchuangyu@gamil.com>
2018-01-22 11:48:38 +0800

    update citation trend plot

--- R/citation_trend.R
+++ R/citation_trend.R
@@ -12,15 +12,27 @@
 ##' @importFrom ggplot2 geom_point
 ##' @importFrom ggplot2 xlab
 ##' @importFrom ggplot2 ylab
+##' @importFrom ggplot2 theme
+##' @importFrom ggplot2 geom_text
+##' @importFrom ggplot2 ylim
+##' @importFrom ggplot2 element_blank
 ##' @export
 ##' @author guangchuang yu
 plot_citation_trend <- function(id, pubid) {
     df <- get_article_cite_history(id, pubid)
     df$year <- as.factor(df$year)
     title <- paste('Cited by', sum(df$cites))
-    p <- ggplot(df, aes_(~year, ~cites)) + geom_segment(aes_(xend=~year, yend=0)) + geom_point(size=3, color='firebrick') +
-        theme_minimal() + xlab(NULL) + ylab(NULL) + labs(title=title, caption="data from Google Scholar")
-    return(p)
+    ggplot(df, aes_(~year, ~cites)) +
+        geom_segment(aes_(xend=~year, yend=0), size=1, color='grey') +
+        geom_point(size=8, color="#96B56C") +
+    geom_text(aes(label=~cites), size=3) +
+        ylim(NA, round(max(df$cites) * 1.1)) +
+        xlab(NULL) +
+        ylab(NULL) +
+        labs(title=title, caption="data from Google Scholar") +
+        theme_minimal() +
+        theme(panel.grid.major.x = element_blank(),
+              panel.grid.minor = element_blank())
 }
 
 
